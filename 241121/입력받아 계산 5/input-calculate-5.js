41ee598dac61a09e4b929c0f0801a095d9e1adab
Jun <122501996+Jun279@users.noreply.github.com>
2024-11-21 10:52:51 +0900

    Title: 입력받아 계산 5, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

--- 241121/입력받아 계산 5/input-calculate-5.js
+++ 241121/입력받아 계산 5/input-calculate-5.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let arr = fs.readFileSync(0).toString();
 let numberArr = arr.split(" ");
 
-let a = parseINf(numberArr[0]);
-let b = parseINf(numberArr[1]);
+let a = parseInt(numberArr[0]);
+let b = parseInt(numberArr[1]);
 
 console.log(a+b);
