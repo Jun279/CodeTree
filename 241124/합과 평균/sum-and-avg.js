335a532752bfe4635939b48fe49a99f66a1e6077
Jun <122501996+Jun279@users.noreply.github.com>
2024-11-24 19:50:26 +0900

    Title: 합과 평균, Time: 192ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241124/합과 평균/sum-and-avg.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString();
+let number = input.split(" ");
+let a = parseInt(number[0]);
+let b = parseInt(number[1]);
+
+console.log(`${a+b} ${((a+b)/2).toFixed(1)}`);
