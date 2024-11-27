b486bc404e8ed6e2556241d30aec8fd2d03eee5c
Jun <122501996+Jun279@users.noreply.github.com>
2024-11-27 14:42:09 +0900

    Title: 숫자의 조건 여부, Time: 174ms, Memory: 15MB, Status: Passed - CodeTree

--- 241127/숫자의 조건 여부/numbers-condition.js
+++ 241127/숫자의 조건 여부/numbers-condition.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = parseInt(fs.readFileSync(0).toString);
+const input = parseInt(fs.readFileSync(0).toString());
 if(input>=113) {
     console.log(1);
 } else {
