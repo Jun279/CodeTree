ebab6063d458c9fbab728d71f4ec691f4418690b
Jun <122501996+Jun279@users.noreply.github.com>
2024-11-28 15:09:15 +0900

    Title: 살 수 있는 물건 2, Time: 162ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241128/살 수 있는 물건 2/things-able-to-buy-2.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = Number(fs.readFileSync(0).toString());
+if(input >= 3000) {
+    console.log("book");
+} else if(input >=1000) {
+    console.log("mask");
+} else if(input>=500) {
+    console.log("pen");
+} else {
+    console.log("no");
+}
